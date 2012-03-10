6f84449bc36c694b1f61e191f820db328b2db621
yu <yumm007@gmail.com>
2012-03-10 22:59:31 +0800

    cd.lisp v0.1

---
+++ cd.lisp
@@ -0,0 +1,22 @@
+(defun make-cd(title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defvar *db* nil)
+
+(defun add-record(cd) (push cd *db*))
+
+(defun dump-cd()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]")))
